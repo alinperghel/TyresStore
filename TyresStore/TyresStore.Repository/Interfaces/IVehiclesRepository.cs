fab37b4db4575b76d21b69879c922d8626372650
Alin Perghel <a.perghel@gmail.com>
2018-03-05 13:18:51 +0200

    work 05.03.2018

---
+++ TyresStore/TyresStore.Repository/Interfaces/IVehiclesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TyresStore.Repository.Models;
+
+namespace TyresStore.Repository.Interfaces
+{
+    interface IVehiclesRepository
+    {
+        Vehicle GetVehicleById(int vehicleId);
+        List<Vehicle> GetVehicles();
+    }
+}
